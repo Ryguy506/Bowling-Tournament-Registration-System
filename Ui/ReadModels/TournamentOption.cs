6ea992497b6400a086177c4447d139f4fac794b2
BraTho <BTHOMPSON03@mynbcc.ca>
2026-03-19 20:29:29 -0300

    Create and Edit Complete

---
+++ Ui/ReadModels/TournamentOption.cs
@@ -0,0 +1,17 @@
+﻿namespace Bowling_Tournament_Registration_System.Ui.ReadModels
+{
+    public class TournamentOption
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Location { get; set; }
+
+        public int RegisteredCount { get; set; }
+
+        public int Capacity { get; set; }
+    }
+}
